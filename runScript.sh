641b3260fb019a0b2593d0ab5cf2d179a99cffe4
rspijkers <rik.spijkers@gmail.com>
2022-03-17 11:48:54 +0100

    Fix job id in bash steering, remove output dir creation in code dir

--- runScript.sh
+++ runScript.sh
@@ -6,11 +6,11 @@ ENVIRONMENT="VO_ALICE@AliPhysics::vAN-20180913-1,VO_ALICE@pythia::v8230-1" # Nik
 # ENVIRONMENT="O2/latest,pythia/latest" # Home
 echo $ENVIRONMENT
 
-cd /user/rspijker/code # Nikhef
-if [[ ! -d "output" ]]
-then 
-    mkdir output
-fi
+# cd /user/rspijker/code # Nikhef
+# if [[ ! -d "output" ]]
+# then 
+#     mkdir output
+# fi
 # make pythia script
 eval "alienv setenv $ENVIRONMENT -c make ssbar_correlations"
 for i in {1..10}
--- submitScript.sh
+++ submitScript.sh
@@ -11,6 +11,10 @@ ENVIRONMENT="VO_ALICE@AliPhysics::vAN-20180913-1,VO_ALICE@pythia::v8230-1" # Nik
 # TODO: add outputfile location as option of cpp script? 
 # so we can steer it to /dcache/ at nikhef, or something else.
 # we can then also let the output path depend on where we run it, home or nikhef
-OUTPUTFILE="/data/alice/rspijker/output/PythiaResults$PBS_JOBID.root"
+
+# get job id and take only the numeric part (leave out the .burrell.nikhef.nl)
+JOB_ID=$(echo $PBS_JOBID | cut -d'.' -f 1)
+
+OUTPUTFILE="/data/alice/rspijker/output/PythiaResults$JOB_ID.root"
 
 eval "alienv setenv $ENVIRONMENT -c ./ssbar_correlations $OUTPUTFILE"
