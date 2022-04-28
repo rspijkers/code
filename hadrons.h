bb731c6534ffbe4d703c3b8daf1cf4058d9951f8
rspijkers <rik.spijkers@gmail.com>
2022-04-28 12:08:17 +0200

    fix hadron class and move from pointers to normal

--- hadrons.h
+++ hadrons.h
@@ -1,3 +1,6 @@
+#pragma once
+#ifndef HADRONS_
+#define HADRONS_
 // container for all info regarding a specific hadron, including it the info on its antiparticle
 class Hadron {
     private:
@@ -32,9 +35,6 @@ class Hadron {
             latex = (TString) _latex;
         }
 
-        // do not allow copying
-        Hadron(const Hadron&) = delete;
-
         //// GETTERS ////
         TString getName() {return name;}
         TString getAntiName() {return antiname;}
@@ -44,15 +44,17 @@ class Hadron {
         TString getAntiLatex() {return antilatex;}
 };
 
+
 // declare the hadrons needed for analysis, use pointers so we only create one instance per hadron -- no need for copying
-static Hadron *Kzero = new Hadron("K0", "K0bar", 311, "K^{0}", "#bar{K^{0}}");
-static Hadron *Kminus = new Hadron("K-", "K+", -321, "K^{-}", "K^{+}");
-static Hadron *Lambda = new Hadron("Lambda", "Lambdabar", 3122, "#Lambda", "#bar{#Lambda}");
-static Hadron *Sigmazero = new Hadron("Sigma0", "Sigma0bar", 3212, "#Sigma^{0}", "#bar{#Sigma^{0}}");
-static Hadron *Sigmaminus = new Hadron("Sigma-", "Sigma-bar", 3112, "#Sigma^{-}", "#bar{#Sigma^{-}}");
-static Hadron *Sigmaplus = new Hadron("Sigma+", "Sigma+bar", 3222, "#Sigma^{+}", "#bar{#Sigma^{+}}");
-static Hadron *Xizero = new Hadron("Xi0", "Xi0bar", 3322, "#Xi^{0}", "#bar{#Xi^{0}}");
-static Hadron *Ximinus = new Hadron("Xi-", "Xi+", 3312, "#Xi^{-}", "#Xi^{+}");
-static Hadron *Omegaminus = new Hadron("Omega-", "Omega+", 3334, "#Omega^{-}", "#Omega^{+}");
-static Hadron *Kzeroshort = new Hadron("K0_S", 310, "K^{0}_{S}");
-static Hadron *Kzerolong = new Hadron("K0_L", 130, "K^{0}_{L}");
+static Hadron Kzero = Hadron("K0", "K0bar", 311, "K^{0}", "#bar{K^{0}}");
+static Hadron Kminus = Hadron("K-", "K+", -321, "K^{-}", "K^{+}");
+static Hadron Lambda = Hadron("Lambda", "Lambdabar", 3122, "#Lambda", "#bar{#Lambda}");
+static Hadron Sigmazero = Hadron("Sigma0", "Sigma0bar", 3212, "#Sigma^{0}", "#bar{#Sigma^{0}}");
+static Hadron Sigmaminus = Hadron("Sigma-", "Sigma-bar", 3112, "#Sigma^{-}", "#bar{#Sigma^{-}}");
+static Hadron Sigmaplus = Hadron("Sigma+", "Sigma+bar", 3222, "#Sigma^{+}", "#bar{#Sigma^{+}}");
+static Hadron Xizero = Hadron("Xi0", "Xi0bar", 3322, "#Xi^{0}", "#bar{#Xi^{0}}");
+static Hadron Ximinus = Hadron("Xi-", "Xi+", 3312, "#Xi^{-}", "#Xi^{+}");
+static Hadron Omegaminus = Hadron("Omega-", "Omega+", 3334, "#Omega^{-}", "#Omega^{+}");
+static Hadron Kzeroshort = Hadron("K0_S", 310, "K^{0}_{S}");
+static Hadron Kzerolong = Hadron("K0_L", 130, "K^{0}_{L}");
+#endif
