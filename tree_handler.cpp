973af5469ae49d601e99c989860489c0fcff29ae
rspijkers <rik.spijkers@gmail.com>
2022-05-23 16:16:28 +0200

    change pointer convention, add pdg QA check in tree handler, add more dphi correlation histo's

--- tree_handler.cpp
+++ tree_handler.cpp
@@ -5,6 +5,7 @@
 #include "TTree.h"
 #include "TPad.h"
 #include <vector>
+#include <set>
 #include <unordered_map>
 #include "hadrons.h"
 #include "helperfunctions.h"
@@ -14,17 +15,25 @@
 void tree_handler() {
     TH1::SetDefaultSumw2(); // make sure errors are propagated in histo's
     
-    TFile *inputfile = new TFile("output/ssbar_5M_14TeV_monash_ppbar_noEta.root", "READ");
-    TTree *tree = (TTree*) inputfile->Get("tree");
+    TFile* inputfile = new TFile("output/ssbar_5M_14TeV_monash_ppbar_noEta.root", "READ");
+    TTree* tree = (TTree*) inputfile->Get("tree");
     // tree->Print();
     // tree->Show(0);
 
-    TFile *outputfile = new TFile("output/plots_monash_ppbar_test.root", "RECREATE");
+    // Check to see if the under/overflow bins in the pdg are empty. If not, smt funky is going on
+    TH1D* hPDGTrigger = (TH1D*) inputfile->Get("hPDG");
+    Int_t nPDGbins = hPDGTrigger->GetNbinsX();
+    if(hPDGTrigger->GetBinContent(0) > 0 || hPDGTrigger->GetBinContent(nPDGbins + 1) > 0){
+        std::cout << "WARNING: non-empty underflow and/or overflow bins in the PDG histogram!!! This means we are not accounting for all particle species. Please investigate" << std::endl;
+    }
+
+    TFile* outputfile = new TFile("output/plots_monash_ppbar_test.root", "RECREATE");
 
     // Kinematic cuts
     const Double_t maxEtaTrigger = 2.0;
     const Double_t maxEtaAssoc = 3.0;
     // std::cout << "We are using eta cuts: maxEtaTrigger = " << maxEtaTrigger << " and maxEtaAssoc = " << maxEtaAssoc << std::endl;
+    std::set<Int_t> uniqueTriggerPDGs, uniqueAssocPDGs; // to keep track of possible PDG
 
     // Set branch address, so we can use the variables when we GetEntry()
     Int_t pdgTrigger;
@@ -58,25 +67,41 @@ void tree_handler() {
         Double_t chargedKaonSig = 0;
     };
 
-    TH1D *hInclusiveTrigger = new TH1D("hInclusiveTrigger", "Inclusive transverse momentum spectrum for trigger hadrons", 100, 0, 25);
-    TH1D *hInclusiveAssoc = new TH1D("hInclusiveAssoc", "Inclusive transverse momentum spectrum for associated hadrons", 100, 0, 10);
-    TH1D *hEtaTrigger = new TH1D("hEtaTrigger", "Pseudorapidity spectrum for trigger hadrons", 100, -10, 10);
-    TH1D *hEtaAssoc = new TH1D("hEtaAssoc", "Pseudorapidity spectrum for associated hadrons", 100, -10, 10);
-    TH1D *hYTrigger = new TH1D("hYTrigger", "Rapidity spectrum for trigger hadrons", 100, -10, 10);
-    TH1D *hYAssoc = new TH1D("hYAssoc", "Rapidity spectrum for associated hadrons", 100, -10, 10);
-    TH1D *hEtaNormal = new TH1D("hEtaNormal", "Pseudorapidity spectrum for 'normal' baryons", 100, -10, 10);
-    TH1D *hEtaAnti = new TH1D("hEtaAnti", "Pseudorapidity spectrum for anti baryons", 100, -10, 10);
+    TH1D* hInclusiveTrigger = new TH1D("hInclusiveTrigger", "Inclusive transverse momentum spectrum for trigger hadrons", 100, 0, 25);
+    TH1D* hInclusiveAssoc = new TH1D("hInclusiveAssoc", "Inclusive transverse momentum spectrum for associated hadrons", 100, 0, 10);
+    TH1D* hEtaTrigger = new TH1D("hEtaTrigger", "Pseudorapidity spectrum for trigger hadrons", 100, -10, 10);
+    TH1D* hEtaAssoc = new TH1D("hEtaAssoc", "Pseudorapidity spectrum for associated hadrons", 100, -10, 10);
+    TH1D* hYTrigger = new TH1D("hYTrigger", "Rapidity spectrum for trigger hadrons", 100, -10, 10);
+    TH1D* hYAssoc = new TH1D("hYAssoc", "Rapidity spectrum for associated hadrons", 100, -10, 10);
+    TH1D* hEtaNormal = new TH1D("hEtaNormal", "Pseudorapidity spectrum for 'normal' baryons", 100, -10, 10);
+    TH1D* hEtaAnti = new TH1D("hEtaAnti", "Pseudorapidity spectrum for anti baryons", 100, -10, 10);
 
     // DeltaPhi for lambda's, to check the difference between pp and ppbar
-    TH1D *hLLdphi = new TH1D("hLLdphi", "deltaphi for L-L", 50, -0.5*PI, 1.5*PI);
-    TH1D *hLLbardphi = new TH1D("hLLbardphi", "deltaphi for L-Lbar", 50, -0.5*PI, 1.5*PI);
-    TH1D *hLbarLdphi = new TH1D("hLbarLdphi", "deltaphi for Lbar-L", 50, -0.5*PI, 1.5*PI);
-    TH1D *hLbarLbardphi = new TH1D("hLbarLbardphi", "deltaphi for Lbar-Lbar", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLLdphi = new TH1D("hLLdphi", "deltaphi for L-L", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLLbardphi = new TH1D("hLLbardphi", "deltaphi for L-Lbar", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarLdphi = new TH1D("hLbarLdphi", "deltaphi for Lbar-L", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarLbardphi = new TH1D("hLbarLbardphi", "deltaphi for Lbar-Lbar", 50, -0.5*PI, 1.5*PI);
+
+    // TODO: implement the same dphi histo's for the following pairs: L-K, L-S^min, K-K
+    TH1D* hLKmindphi = new TH1D("hLKmindphi", "deltaphi for L-Kmin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLKplusdphi = new TH1D("hLKplusdphi", "deltaphi for L-Kplus", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarKmindphi = new TH1D("hLbarKmindphi", "deltaphi for Lbar-Kmin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarKplusdphi = new TH1D("hLbarKplusdphi", "deltaphi for Lbar-Kplus", 50, -0.5*PI, 1.5*PI);
+
+    TH1D* hLSdphi = new TH1D("hLSdphi", "deltaphi for L-Sigmamin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLSbardphi = new TH1D("hLSbardphi", "deltaphi for L-Sigmaminbar", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarSdphi = new TH1D("hLbarSdphi", "deltaphi for Lbar-Sigmamin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hLbarSbardphi = new TH1D("hLbarSbardphi", "deltaphi for Lbar-Sigmaminbar", 50, -0.5*PI, 1.5*PI);
+
+    TH1D* hKminKmindphi = new TH1D("hKminKmindphi", "deltaphi for Kmin-Kmin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hKminKplusdphi = new TH1D("hKminKplusdphi", "deltaphi for Kmin-Kplus", 50, -0.5*PI, 1.5*PI);
+    TH1D* hKplusKmindphi = new TH1D("hKplusKmindphi", "deltaphi for Kplus-Kmin", 50, -0.5*PI, 1.5*PI);
+    TH1D* hKplusKplusdphi = new TH1D("hKplusKplusdphi", "deltaphi for Kplus-Kplus", 50, -0.5*PI, 1.5*PI);
 
 
-    TH1D *hTemp = new TH1D("template", "template", nbins, 0, nbins); 
+    TH1D* hTemp = new TH1D("template", "template", nbins, 0, nbins); 
     // hTemp->SetOption("HIST E");
-    TAxis *ax = hTemp->GetXaxis();
+    TAxis* ax = hTemp->GetXaxis();
     ax->SetBinLabel(1, "K0_S/L");
     for (Int_t i = 0; i < nbins - 1; i++){
         // 0th bin is underflow, 1st bin is K0_S/L, so start with i + 2
@@ -87,16 +112,16 @@ void tree_handler() {
         TString name = hadron.getName();
         TString antiname = hadron.getAntiName();
 
-        TH1D *hss = (TH1D*) hTemp->Clone();
+        TH1D* hss = (TH1D*) hTemp->Clone();
         hss->SetName(name + "_bkg");
         hss->SetTitle("strange strange pairs");
-        TH1D *hssbar = (TH1D*) hTemp->Clone();
+        TH1D* hssbar = (TH1D*) hTemp->Clone();
         hssbar->SetName(name + "_sig");
         hssbar->SetTitle("strange anti-strange pairs");
-        TH1D *hsbars = (TH1D*) hTemp->Clone();
+        TH1D* hsbars = (TH1D*) hTemp->Clone();
         hsbars->SetName(antiname + "_sig");
         hsbars->SetTitle("anti-strange strange pairs");
-        TH1D *hsbarsbar = (TH1D*) hTemp->Clone();
+        TH1D* hsbarsbar = (TH1D*) hTemp->Clone();
         hsbarsbar->SetName(antiname + "_bkg");
         hsbarsbar->SetTitle("anti-strange anti-strange pairs");
         
@@ -135,6 +160,7 @@ void tree_handler() {
     for(Long64_t i = 0; i < nentries; i++) {
         tree->GetEntry(i);
         // inclusive spectra before kine cuts
+        uniqueTriggerPDGs.insert(pdgTrigger); // will only insert if unique
         Hadron trigger = map[pdgTrigger].hadron;
         hInclusiveTrigger->Fill(pTTrigger);
         hEtaTrigger->Fill(etaTrigger);
@@ -157,8 +183,8 @@ void tree_handler() {
         if(pdgTrigger > 1000) {hEtaNormal->Fill(etaTrigger);}
         else if(pdgTrigger < -1000) {hEtaAnti->Fill(etaTrigger);}
 
-        TH1D *hSig = map[pdgTrigger].hSig;
-        TH1D *hBkg = map[pdgTrigger].hBkg;
+        TH1D* hSig = map[pdgTrigger].hSig;
+        TH1D* hBkg = map[pdgTrigger].hBkg;
         Int_t tStrangeness = map[pdgTrigger].strangeness;
         map[pdgTrigger].ntriggers++;
         // loop over assoc
@@ -168,6 +194,7 @@ void tree_handler() {
             Double_t pt = (*pTAssoc)[j];
             Hadron assoc = map[pdg].hadron;
             if(!isDuplicate){
+                uniqueAssocPDGs.insert(pdg); // will only insert if unique
                 hInclusiveAssoc->Fill(pt);
                 hEtaAssoc->Fill(eta);
                 y = rapidityFromEta(eta, pt, assoc.getMass()/1000.); // convert mass to GeV
@@ -198,11 +225,27 @@ void tree_handler() {
 
             // make some deltaphi plots for lambda correlations
             if(pdgTrigger == Lambda.getPDG()){
-                if(pdg == Lambda.getPDG()){hLLdphi->Fill((*deltaPhi)[j]);}
-                else if(pdg == Lambda.getAntiPDG()){hLLbardphi->Fill((*deltaPhi)[j]);}
+                if(pdg == Lambda.getPDG()){hLLdphi->Fill((*deltaPhi)[j]);} // LL
+                else if(pdg == Lambda.getAntiPDG()){hLLbardphi->Fill((*deltaPhi)[j]);} // LLbar
+                else if(pdg == Kminus.getPDG()){hLKmindphi->Fill((*deltaPhi)[j]);} // LK
+                else if(pdg == Kminus.getAntiPDG()){hLKplusdphi->Fill((*deltaPhi)[j]);} // LKbar
+                else if(pdg == Sigmaminus.getPDG()){hLSdphi->Fill((*deltaPhi)[j]);} // LS
+                else if(pdg == Sigmaminus.getAntiPDG()){hLSbardphi->Fill((*deltaPhi)[j]);} // LSbar
             } else if(pdgTrigger == Lambda.getAntiPDG()){
-                if(pdg == Lambda.getPDG()){hLbarLdphi->Fill((*deltaPhi)[j]);}
-                else if(pdg == Lambda.getAntiPDG()){hLbarLbardphi->Fill((*deltaPhi)[j]);}
+                if(pdg == Lambda.getPDG()){hLbarLdphi->Fill((*deltaPhi)[j]);} // LbarL
+                else if(pdg == Lambda.getAntiPDG()){hLbarLbardphi->Fill((*deltaPhi)[j]);} // LbarLbar
+                else if(pdg == Kminus.getPDG()){hLbarKmindphi->Fill((*deltaPhi)[j]);} // LbarK
+                else if(pdg == Kminus.getAntiPDG()){hLbarKplusdphi->Fill((*deltaPhi)[j]);} // LbarKbar
+                else if(pdg == Sigmaminus.getPDG()){hLbarSdphi->Fill((*deltaPhi)[j]);} // LbarS
+                else if(pdg == Sigmaminus.getAntiPDG()){hLbarSbardphi->Fill((*deltaPhi)[j]);} // LbarSbar
+            }
+            // Kaon corr.
+            else if(pdgTrigger == Kminus.getPDG()){
+                if(pdg == Kminus.getPDG()){hKminKmindphi->Fill((*deltaPhi)[j]);} // KminKmin
+                else if(pdg == Kminus.getAntiPDG()){hKminKplusdphi->Fill((*deltaPhi)[j]);} // KminKplus
+            } else if(pdgTrigger == Kminus.getAntiPDG()){
+                if(pdg == Kminus.getPDG()){hKplusKmindphi->Fill((*deltaPhi)[j]);} // KplusKmin
+                else if(pdg == Kminus.getAntiPDG()){hKplusKplusdphi->Fill((*deltaPhi)[j]);} // KplusKplus
             }
         }
     }
@@ -251,7 +294,11 @@ void tree_handler() {
         std::cout << hadron.getAntiName() << ": " << ratio1 << " +/- " << errAnti << " found with ntriggers = " << map[antipdg].ntriggers << std::endl;
     }
 
-    // TODO: draw some lambda deltaphi correlations in the same histo?
+    // list all the possible pdgs for both trigger and assoc:
+    std::cout << "number of unique strange trigger hadrons found, should be " << 2*nbins << ": " << uniqueTriggerPDGs.size() << std::endl;
+    std::cout << "number of unique strange trigger hadrons found, should be " << 2*nbins << ": " << uniqueAssocPDGs.size() << std::endl;
+    // for(Int_t pdg : uniqueTriggerPDGs){std:: cout << pdg << std::endl;}
+    // for(Int_t pdg : uniqueAssocPDGs){std:: cout << pdg << std::endl;}
     
     inputfile->Close();
     outputfile->Write();
