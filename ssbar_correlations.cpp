e700bea4d9915682635107700545063ac50ec417
rspijkers <rik.spijkers@gmail.com>
2022-06-01 11:11:16 +0200

    Add a net strangeness per event check, other minor improvements

--- ssbar_correlations.cpp
+++ ssbar_correlations.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <cmath> // needed for modulo in deltaPhi calc
+#include <vector> // needed for a variable array
+#include <cstring> // string/char handling
+#include <chrono> // for measuring performance of script
 #include "Pythia8/Pythia.h"
 #include "TFile.h"
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TH1I.h"
 #include "TTree.h"
-#include "THnSparse.h" // obsolete?
-#include <cmath> // needed for modulo in deltaPhi calc
-#include <vector> // needed for a variable array
-#include <cstring> // string/char handling
-#include <chrono> // for measuring performance of script
+#include "helperfunctions.h"
 
 #define PI 3.14159265
 
 using namespace Pythia8;
+using std::cout; using std::endl;
 
 bool IsStrange(Int_t particlepdg) {
 	// Checks if given pdg code is that of a (anti)strange hadron, returns True/False
@@ -50,7 +51,7 @@ int main(int argc, char** argv)
 	Pythia pythia;
 
 	// PYTHIA SETTINGS
-	pythia.readFile("ssbar_skands_mode2.cmnd");
+	pythia.readFile("pythia_settings/ssbar_monash.cmnd");
 	Int_t nEvents = pythia.mode("Main:numberOfEvents");
 
 	Int_t processid = getpid();
@@ -67,13 +68,15 @@ int main(int argc, char** argv)
 
 	TFile* outFile = new TFile(argv[1], "CREATE"); // doesn't open file if it already exists 
 	if(!outFile->IsOpen()) { // if output file isn't opened, abort program
-		cerr << "Error: File " << argv[1] << "is not opened, perhaps because it already exists. Aborting script.";
+		cerr << "Error: File " << argv[1] << " is not opened, perhaps because it already exists. Aborting script.";
 		return 1;
 	}
 
 	// Output histo's
   	TH2D *hEtaPt = new TH2D("hEtaPt","p_{T} vs #eta for all particles;#eta;p_{T} (GeV/c)", 40, -4., 4., 50, 0, 10);
 	TH1D *hPDG = new TH1D("hPDG", "PDG code for trigger strange hadrons", 8000, -4000, 4000); // use Double_t to get around maximum bin content of Int_t
+	// TODO: make the same PDG histogram for the associated hadrons
+	TH1I *hStrangenessPerEvent = new TH1I("hStrangenessPerEvent", "net strangeness per event", 21, -10.5, 10.5);
 
 	// TODO: create tree to save all the data of trigger/assoc pairs to
 	// not sure how to handle multiple assocs per trigger
@@ -101,41 +104,54 @@ int main(int argc, char** argv)
 		if(!pythia.next()) continue;
 
 		int nPart = pythia.event.size();
+		int strangenessPerEvent = 0;
 		
 		for(int iPart = 0; iPart < nPart; iPart++) {
       		const Particle &part = pythia.event[iPart];
+			if(!part.isFinal()) continue; // final state particle 
 			partpT = part.pT();
 			parteta = part.eta();
+			partpdg = part.id();
 			hEtaPt->Fill(parteta,partpT);
-			if(partpT > pTminTrigg && part.isFinal()) { // final state particle + kine cuts
-				// we have identified a potential trigger that satisfies the kinematic requirements
-				partpdg = part.id();
-				if(IsStrange(partpdg)) {
-					// If we get this far with the trigger particle, we will correlate it with other strange hadrons
-					// In order to be able to normalize, we need to keep track of how many triggers we have for each hadron, because in the next part we will fill the trigger particle info for each pair, which could be more than one.
-					hPDG->Fill((Double_t) partpdg);
-
-					// Clear the vectors with the associated/correlation variables
-					pdgAssoc.clear(); pTAssoc.clear(); etaAssoc.clear(); deltaPhi.clear(); deltaEta.clear();
-					for(int jPart = 0; jPart < nPart; jPart++) {
-						const Particle &part2 = pythia.event[jPart];
-						Double_t part2pT = part2.pT();
-						Double_t part2eta = part2.eta();
-						Int_t part2pdg = part2.id();
-						if(IsStrange(part2pdg) && part2.isFinal() && partpT > part2pT && part2pT > pTminAssoc){
-							Double_t dPhi = std::fmod(part.phi() - part2.phi() + 2.5*PI, 2*PI) - 0.5*PI; 
-							Double_t dEta = parteta - part2eta;
-							pdgAssoc.push_back(part2pdg);
-							pTAssoc.push_back(part2pT);
-							etaAssoc.push_back(part2eta);
-							deltaPhi.push_back(dPhi);
-							deltaEta.push_back(dEta);
-						}
-					} // end assoc loop
-					tree->Fill();
-				}
+			int netStrange = strangenessFromPDG(partpdg); // not yet net strangeness
+			if(netStrange == 0) continue; // strangeness check
+			if(iEvent < 10) cout << iEvent << ": " << partpdg << endl;
+			// check the net strangeness
+			if(partpdg == 310 || partpdg == 130) netStrange = 0; // K0_S/L
+			else if(abs(partpdg) == 321 || abs(partpdg) == 431 || abs(partpdg) == 311) netStrange *= -1; // switch if kaon or D_s, PDG convention
+			else if(netStrange == 2 && getDigitN(partpdg, 3) == 0) {
+				netStrange = 0; // if a meson has 2 strange quarks, it is an ssbar state thus net zero strangeness
 			}
+			int signpdg = 1;
+			if(partpdg < 0) signpdg = -1;
+			strangenessPerEvent += netStrange * signpdg;
+
+			if(partpT < pTminTrigg) continue; // kine cuts
+			// we have identified a strange trigger that satisfies the kinematic requirements
+			// If we get this far with the trigger particle, we will correlate it with other strange hadrons
+			// In order to be able to normalize, we need to keep track of how many triggers we have for each hadron, because in the next part we will fill the trigger particle info for each pair, which could be more than one.
+			hPDG->Fill((Double_t) partpdg);
+
+			// Clear the vectors with the associated/correlation variables
+			pdgAssoc.clear(); pTAssoc.clear(); etaAssoc.clear(); deltaPhi.clear(); deltaEta.clear();
+			for(int jPart = 0; jPart < nPart; jPart++) {
+				const Particle &part2 = pythia.event[jPart];
+				Double_t part2pT = part2.pT();
+				Double_t part2eta = part2.eta();
+				Int_t part2pdg = part2.id();
+				if(IsStrange(part2pdg) && part2.isFinal() && partpT > part2pT && part2pT > pTminAssoc){
+					Double_t dPhi = std::fmod(part.phi() - part2.phi() + 2.5*PI, 2*PI) - 0.5*PI; 
+					Double_t dEta = parteta - part2eta;
+					pdgAssoc.push_back(part2pdg);
+					pTAssoc.push_back(part2pT);
+					etaAssoc.push_back(part2eta);
+					deltaPhi.push_back(dPhi);
+					deltaEta.push_back(dEta);
+				}
+			} // end assoc loop
+			tree->Fill();
 		} // end trigger loop
+		hStrangenessPerEvent->Fill(strangenessPerEvent);
 	} // end event loop
 
 	outFile->Write();
