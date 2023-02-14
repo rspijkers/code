c3f4f6d32ae403ca8f41edbdd94ef03228c0bd42
rspijkers <rik.spijkers@gmail.com>
2023-02-14 11:52:56 +0100

    redirect logfiles

--- PythiaEventGen/runScript.sh
+++ PythiaEventGen/runScript.sh
@@ -11,6 +11,8 @@ echo $ENVIRONMENT
 OUTPUTPATH="/dcache/alice/rspijker/ModelStudyJan"
 OUTPUTDIR="Monash_pp_100M_14TeV"
 [ ! -d "$OUTPUTPATH/$OUTPUTDIR" ] && mkdir -p "$OUTPUTPATH/$OUTPUTDIR" && echo "outputdir created"
+LOGPATH="$OUTPUTPATH/$OUTPUTDIR/logs"
+[ ! -d "$LOGPATH" ] && mkdir -p "$LOGPATH"
 
 # pythia config file
 PYTHIACONFIG="pythia_settings/ssbar_monash.cmnd"
@@ -26,5 +28,5 @@ fi
 for i in {1..10}
 do
     echo "submit number $i"
-    qsub -F "$OUTPUTPATH/$OUTPUTDIR $PYTHIACONFIG $i" submitScript.sh
+    qsub -F "$OUTPUTPATH/$OUTPUTDIR $PYTHIACONFIG $i" submitScript.sh -o $LOGPATH -e $LOGPATH
 done
